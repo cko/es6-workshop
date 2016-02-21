66b51683f0f71e7e9843042071cf0fba816c7a8a
Christine Koppelt <cko@users.noreply.github.com>
2016-02-21 15:23:51 +0100

    add react component and styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import GroupedKatas from './grouped-katas.js';
 import {KATAS_URL} from './config';
 
+import React from 'react';
+import KataGroupsComponent from './component/kata-groups'
+class Page extends React.Component {
+      render() {
+              var groupNames = Object.keys(this.props.katas.groups);
+                  return (
+                                <KataGroupsComponent groupNames={groupNames} />
+                                    );
+                    }
+}
+
 new GroupedKatas().load(KATAS_URL)
       .then(groupedKatas => {
-              document.getElementById('app').innerHTML = JSON.stringify(groupedKatas);
-                });
+              //document.getElementById('app').innerHTML = JSON.stringify(groupedKatas);
+              ReactDOM.render(<Page katas={groupedKatas}/>, document.getElementById('app'));
+  });
